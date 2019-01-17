2e0dc1bed4e97f292e528948246dc31e96db42d2
Alexander-Alexandrikov1 <hfghfhfhh151@mail.ru>
2019-01-18 02:53:26 +0300

    пока просто попробовал отрисовать танк

--- Tanks/Tanks/Program.cs
+++ Tanks/Tanks/Program.cs
@@ -12,11 +12,33 @@ namespace Tanks
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            MainForm pc;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            switch (args.Length)
+            {
+                case 0: 
+                    pc = new MainForm(); break;
+                case 1:
+                    pc = new MainForm(Convert.ToInt32(args[0])); break;
+                case 2:
+                    pc = new MainForm(Convert.ToInt32(args[0]), Convert.ToInt32(args[1])); break;
+                case 3:
+                    pc = new MainForm(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), Convert.ToInt32(args[2])); break;
+                case 4:
+                    pc = new MainForm(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3])); break;
+                case 5:
+                    pc = new MainForm(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), Convert.ToInt32(args[3]), Convert.ToInt32(args[4])); break;
+                default:
+                    pc = new MainForm(); break;
+
+            }
+
+            Application.Run(pc);
         }
     }
 }
